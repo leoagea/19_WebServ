c7467db5e5aeee7ff42d783038a08ab25a276b69
Leo AGEA <leoagea@gmail.com>
2024-12-17 22:27:46 +0100

    Update : parse listen, server_name

--- src/config/serverBlock.cpp
+++ src/config/serverBlock.cpp
@@ -6,18 +6,112 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 13:28:47 by lagea             #+#    #+#             */
-/*   Updated: 2024/12/16 16:51:57 by lagea            ###   ########.fr       */
+/*   Updated: 2024/12/17 22:27:15 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "serverBlock.hpp"
 
-serverBlock::serverBlock()
+ServerBlock::ServerBlock(std::vector<t_token> &tokenVec, int *j) : _servername(""), _rootdir(""), _index(""), _acceslogdir(""), _errorlogdir(""), _bodysizelimit(-1) 
+{
+    parseAllServerVariables(tokenVec, j);
+}
+
+ServerBlock::~ServerBlock()
 {
     
 }
 
-serverBlock::~serverBlock()
+void ServerBlock::parseAllServerVariables(std::vector<t_token> &tokenVec, int *j)
 {
+    int i = 0;
+    for (i = *j; i < (int)tokenVec.size() && tokenVec[i].type != closebracket; i++){
+        t_token token = tokenVec[i];
+        if (token.type == keyword && token.value == "listen" && tokenVec[i + 2].type == semicolon){
+            token = tokenVec[i + 1];
+            if (token.type == number){
+                parseListeningPort(token.value);
+            }    
+        }
+        else if (token.type == keyword && token.value == "server_name" && tokenVec[i + 2].type == semicolon){
+            parseServerName(tokenVec[++i]);
+        }
+        else if (token.type == keyword && token.value == "root" && tokenVec[i + 2].type == semicolon){
+            parseServerName(tokenVec[++i]);
+        }
+    }
     
+    if(i == 0)
+        *j += 1;
+    *j = i;
+}
+
+std::vector<int> ServerBlock::getListeningPortsVector() const
+{
+    return _listeningports;
+}
+
+std::string ServerBlock::getServerName() const
+{
+    return _servername;
+}
+
+std::string ServerBlock::getRootDir() const
+{
+    return _rootdir;
+}
+
+void ServerBlock::parseListeningPort(std::string &num)
+{
+    int port = atoi(num.c_str());
+    if (port <= 1023 || port > UINT16_MAX){
+        std::cerr << "Error: config file: port range exceeded 1024 - 65535" << std::endl;
+    }
+    else{
+        if (!_listeningports.empty()){
+            if (find(_listeningports.begin(), _listeningports.end(), port) != _listeningports.end())
+                _listeningports.push_back(port);
+            else
+                std::cerr << "Error: config file: port already used" << std::endl;
+        }
+        else
+            _listeningports.push_back(port);
+    }
+}
+
+void ServerBlock::parseServerName(t_token &token)
+{
+    if (token.type == string)
+        _servername = token.value;
+}
+
+void ServerBlock::parseRootDir(t_token &token)
+{
+    std::cout << "Test    " << token.value << std::endl;
+    if (token.type == string){
+        if (PathChecking::exist(token.value) && PathChecking::isDirectory(token.value))
+            _rootdir = token.value;
+    }
+}
+
+std::ostream &operator<<(std::ostream &out, const ServerBlock &obj)
+{
+    out << BLUE << "Server Block" << RESET << std::endl;
+    
+    out << CYAN << "Listening Port" << RESET << std::endl;
+    std::vector<int>::iterator it;
+    std::vector<int> vec = obj.getListeningPortsVector();
+    int i;
+    for (i = 0, it = vec.begin(); it != vec.end(); i++,  it++){
+        out << "Port "<< i << ":  " << *it << std::endl;
+    }
+    out << CYAN << "Server Name" << RESET << std::endl;
+    out << "Name:  " << obj.getServerName() << std::endl;
+
+    out << CYAN << "Root Dir" << RESET << std::endl;
+    std::string path = obj.getRootDir();
+    out << "Dir:  " << path << std::endl;
+    out << "Exist: " << PathChecking::exist(path) << "  IsDir:  " << PathChecking::isDirectory(path)
+        << "  R:  " << PathChecking::getReadPermission(path) << "  R:  " << PathChecking::getWritePermission(path) << "  R:  " << PathChecking::getExecPermission(path) << std::endl; 
+    return out;
 }
--- src/config/serverBlock.hpp
+++ src/config/serverBlock.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   serverBlock.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 13:28:47 by lagea             #+#    #+#             */
-/*   Updated: 2024/12/16 22:38:01 by lagea            ###   ########.fr       */
+/*   Updated: 2024/12/17 22:19:00 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,44 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <limits>
+#include <algorithm>
 
 #include "locationBlock.hpp"
+#include "tokenizer.hpp"
+#include "PathChecking.hpp"
+#include "../../inc/Color.h"
 
 class locationBlock;
 
-class serverBlock 
+class ServerBlock 
 {
     public:
-        serverBlock();
-        ~serverBlock();
+        ServerBlock(std::vector<t_token> &, int *);
+        ~ServerBlock();
 
+        ServerBlock getServerBlock() const;
+        std::vector<int> getListeningPortsVector() const;
+        std::string getServerName() const;
+        std::string getRootDir() const;
+        
     private:
-       std::set<unsigned int> _listeningports;
-       std::string _servername;
-       std::string _rootdir;
-       std::string _index;
-       std::string _acceslogdir;
-       std::string _errorlogdir;
-       unsigned int _bodysizelimit;
-       std::map<std::string ,class locationBlock> _locationblock;
-       std::map<std::pair<int, std::string>, class locationBlock> _errorpages;
+        std::vector<int> _listeningports; //index 0 is default port
+        std::string _servername;
+        std::string _rootdir;
+        std::string _index;
+        std::string _acceslogdir;
+        std::string _errorlogdir;
+        int _bodysizelimit;
+        std::map<std::string ,class locationBlock> _locationblock;
+        std::map<std::pair<int, std::string>, class locationBlock> _errorpages;
+
+        void parseAllServerVariables(std::vector<t_token> &, int *);
+        void parseListeningPort(std::string &);
+        void parseServerName(t_token &);
+        void parseRootDir(t_token &);
 };
+
+std::ostream &operator<<(std::ostream &, const ServerBlock &);
 
 #endif
