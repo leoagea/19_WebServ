de68efe8ec93ff0af4f9fbbf44c563d2ff3fc4bd
Leo Agea <leoagea@gmail.com>
2024-12-17 01:31:31 +0100

    Update : corrected ifndef

--- src/config/serverBlock.hpp
+++ src/config/serverBlock.hpp
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   serverBlock.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 13:28:47 by lagea             #+#    #+#             */
-/*   Updated: 2024/12/16 17:07:09 by lagea            ###   ########.fr       */
+/*   Updated: 2024/12/16 22:38:01 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __SERVERBLOCK_HPP__
-# define __SERVERBLOC_HPP__
+# define __SERVERBLOCK_HPP__
 
 #include <vector>
 #include <iostream>
