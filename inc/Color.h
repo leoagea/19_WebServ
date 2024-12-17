f73fcf520f9b38af9c95d4489957cb64076cd28f
Leo AGEA <leoagea@gmail.com>
2024-12-17 20:15:27 +0100

    Add : multiple defines for colors

---
+++ inc/Color.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Color.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 19:23:24 by lagea             #+#    #+#             */
+/*   Updated: 2024/12/17 19:24:23 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __COLOR_H__
+# define __COLOR_H__
+
+#define ANSI_RESET     "\033[0m"
+#define ANSI_BOLD      "\033[1m"
+#define ANSI_FAINT     "\033[2m"
+#define ANSI_ITALIC    "\033[3m"
+#define ANSI_UNDERLINE "\033[4m"
+#define ANSI_BLINK     "\033[5m"
+
+#define ANSI_BLACK     "\033[30m"
+#define ANSI_RED       "\033[31m"
+#define ANSI_GREEN     "\033[32m"
+#define ANSI_YELLOW    "\033[33m"
+#define ANSI_BLUE      "\033[34m"
+#define ANSI_MAGENTA   "\033[35m"
+#define ANSI_CYAN      "\033[36m"
+#define ANSI_WHITE     "\033[37m"
+
+// Versions "intenses" ou claires
+#define ANSI_BBLACK    "\033[90m"
+#define ANSI_BRED      "\033[91m"
+#define ANSI_BGREEN    "\033[92m"
+#define ANSI_BYELLOW   "\033[93m"
+#define ANSI_BBLUE     "\033[94m"
+#define ANSI_BMAGENTA  "\033[95m"
+#define ANSI_BCYAN     "\033[96m"
+#define ANSI_BWHITE    "\033[97m"
+
+
+#endif
