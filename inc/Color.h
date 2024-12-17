3e80479add2b8199f0c61bda4791375fabb0d6d7
Leo AGEA <leoagea@gmail.com>
2024-12-17 21:03:20 +0100

    Update : remove ANSI from color define names

--- inc/Color.h
+++ inc/Color.h
@@ -6,38 +6,38 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 19:23:24 by lagea             #+#    #+#             */
-/*   Updated: 2024/12/17 19:24:23 by lagea            ###   ########.fr       */
+/*   Updated: 2024/12/17 21:02:58 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __COLOR_H__
 # define __COLOR_H__
 
-#define ANSI_RESET     "\033[0m"
-#define ANSI_BOLD      "\033[1m"
-#define ANSI_FAINT     "\033[2m"
-#define ANSI_ITALIC    "\033[3m"
-#define ANSI_UNDERLINE "\033[4m"
-#define ANSI_BLINK     "\033[5m"
+#define RESET     "\033[0m"
+#define BOLD      "\033[1m"
+#define FAINT     "\033[2m"
+#define ITALIC    "\033[3m"
+#define UNDERLINE "\033[4m"
+#define BLINK     "\033[5m"
 
-#define ANSI_BLACK     "\033[30m"
-#define ANSI_RED       "\033[31m"
-#define ANSI_GREEN     "\033[32m"
-#define ANSI_YELLOW    "\033[33m"
-#define ANSI_BLUE      "\033[34m"
-#define ANSI_MAGENTA   "\033[35m"
-#define ANSI_CYAN      "\033[36m"
-#define ANSI_WHITE     "\033[37m"
+#define BLACK     "\033[30m"
+#define RED       "\033[31m"
+#define GREEN     "\033[32m"
+#define YELLOW    "\033[33m"
+#define BLUE      "\033[34m"
+#define MAGENTA   "\033[35m"
+#define CYAN      "\033[36m"
+#define WHITE     "\033[37m"
 
 // Versions "intenses" ou claires
-#define ANSI_BBLACK    "\033[90m"
-#define ANSI_BRED      "\033[91m"
-#define ANSI_BGREEN    "\033[92m"
-#define ANSI_BYELLOW   "\033[93m"
-#define ANSI_BBLUE     "\033[94m"
-#define ANSI_BMAGENTA  "\033[95m"
-#define ANSI_BCYAN     "\033[96m"
-#define ANSI_BWHITE    "\033[97m"
+#define BBLACK    "\033[90m"
+#define BRED      "\033[91m"
+#define BGREEN    "\033[92m"
+#define BYELLOW   "\033[93m"
+#define BBLUE     "\033[94m"
+#define BMAGENTA  "\033[95m"
+#define BCYAN     "\033[96m"
+#define BWHITE    "\033[97m"
 
 
 #endif
