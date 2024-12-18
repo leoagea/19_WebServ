3503b8d73dab318167f21748aa057a6f88c7b759
Leo AGEA <leoagea@gmail.com>
2024-12-18 19:16:37 +0100

    Update : add root, index, acceslog, errorslog and bodysize parsing

--- src/config/serverBlock.cpp
+++ src/config/serverBlock.cpp
@@ -6,13 +6,13 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 13:28:47 by lagea             #+#    #+#             */
-/*   Updated: 2024/12/17 22:27:15 by lagea            ###   ########.fr       */
+/*   Updated: 2024/12/18 19:13:32 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "serverBlock.hpp"
 
-ServerBlock::ServerBlock(std::vector<t_token> &tokenVec, int *j) : _servername(""), _rootdir(""), _index(""), _acceslogdir(""), _errorlogdir(""), _bodysizelimit(-1) 
+ServerBlock::ServerBlock(std::vector<t_token> &tokenVec, int *j) : _servername(""), _rootdir(""), _index(""), _acceslogdpath(""), _errorlogpath(""), _bodysizelimit(-1) 
 {
     parseAllServerVariables(tokenVec, j);
 }
@@ -37,7 +37,19 @@ void ServerBlock::parseAllServerVariables(std::vector<t_token> &tokenVec, int *j
             parseServerName(tokenVec[++i]);
         }
         else if (token.type == keyword && token.value == "root" && tokenVec[i + 2].type == semicolon){
-            parseServerName(tokenVec[++i]);
+            parseRootDir(tokenVec[++i]);
+        }
+        else if (token.type == keyword && token.value == "index" && tokenVec[i + 2].type == semicolon){
+            parseIndex(tokenVec[++i]);
+        }
+        else if (token.type == keyword && token.value == "access_log" && tokenVec[i + 2].type == semicolon){
+            parseAccesLogPath(tokenVec[++i]);
+        }
+        else if (token.type == keyword && token.value == "errors_log" && tokenVec[i + 2].type == semicolon){
+            parseErrorsLogPath(tokenVec[++i]);
+        }
+        else if (token.type == keyword && token.value == "limit_body_size" && tokenVec[i + 2].type == semicolon){
+            parseLimitBodySize(tokenVec[++i]);
         }
     }
     
@@ -59,6 +71,56 @@ std::string ServerBlock::getServerName() const
 std::string ServerBlock::getRootDir() const
 {
     return _rootdir;
+}
+
+std::string ServerBlock::getIndex() const
+{
+    return _index;    
+}
+
+std::string ServerBlock::getAccesLogFilePath() const
+{
+    return _acceslogdpath;
+}
+
+//Throw a runtime_error if file failed to be opened
+std::ofstream *ServerBlock::getAccessLogStream() const
+{
+    std::ofstream *file = new std::ofstream;
+    std::string path = _acceslogdpath;
+    if ((PathChecking::exist(path)) && PathChecking::isFile(path) && PathChecking::getWritePermission(path)){
+        file->open(path.c_str());
+        if (!file->is_open()){
+            delete file;
+            throw std::runtime_error("Error: failed to open access log file");
+        }
+    }
+    return file;
+}
+
+std::string ServerBlock::getErrorsLogFilePath() const
+{
+    return _errorlogpath;
+}
+
+//Throw a runtime_error if file failed to be opened
+std::ofstream *ServerBlock::getErrorsLogStream() const
+{
+    std::ofstream *file = new std::ofstream;
+    std::string path = _errorlogpath;
+    if ((PathChecking::exist(path)) && PathChecking::isFile(path) && PathChecking::getWritePermission(path)){
+        file->open(path.c_str());
+        if (!file->is_open()){
+            delete file;
+            throw std::runtime_error("Error: failed to open access log file");
+        }
+    }
+    return file;
+}
+
+int ServerBlock::getBodySizeLimit() const
+{
+    return _bodysizelimit;
 }
 
 void ServerBlock::parseListeningPort(std::string &num)
@@ -87,13 +149,71 @@ void ServerBlock::parseServerName(t_token &token)
 
 void ServerBlock::parseRootDir(t_token &token)
 {
-    std::cout << "Test    " << token.value << std::endl;
     if (token.type == string){
-        if (PathChecking::exist(token.value) && PathChecking::isDirectory(token.value))
+        if (PathChecking::isAbsolutePath(token.value) && PathChecking::exist(token.value) && PathChecking::isDirectory(token.value))
             _rootdir = token.value;
     }
 }
 
+void ServerBlock::parseIndex(t_token &token)
+{
+    if (token.type == string){
+        std::string path;
+        
+        if (_rootdir[_rootdir.size()] == '/')
+            path = _rootdir + token.value;
+        else
+            path = _rootdir + "/" + token.value;
+        
+        if (PathChecking::isAbsolutePath(path) && PathChecking::exist(path) && PathChecking::isFile(path) && PathChecking::getReadPermission(path))
+            _index = path;
+    }
+}
+
+void ServerBlock::parseAccesLogPath(t_token &token)
+{
+    if (token.type == string){
+        std::string path = token.value;
+        
+        if (!PathChecking::exist(path)){
+            int fd = open(path.c_str(), O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            if (fd  == -1)
+                std::cerr << "Error: config file: failed to create access log file" << std::endl;
+            else
+                close(fd);
+        }
+        _acceslogdpath = token.value;
+    }
+}
+
+void ServerBlock::parseErrorsLogPath(t_token &token)
+{
+    if (token.type == string){
+        std::string path = token.value;
+        
+        if (!PathChecking::exist(path)){
+            int fd = open(path.c_str(), O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            if (fd  == -1)
+                std::cerr << "Error: config file: failed to create access log file" << std::endl;
+            else
+                close(fd);
+        }
+        _errorlogpath = token.value;
+    }
+}
+
+void ServerBlock::parseLimitBodySize(t_token &token)
+{
+    if (token.type == number){
+        int maxbodysize = atoi(token.value.c_str());
+        if (maxbodysize <= 0 || maxbodysize >= 1025)
+            std::cerr << "Error: config file: max body size range exceeded 1 - 1024" << std::endl;
+        else
+            _bodysizelimit = maxbodysize;
+    }
+}
+
+//Print every private attributes for debugging purpose
 std::ostream &operator<<(std::ostream &out, const ServerBlock &obj)
 {
     out << BLUE << "Server Block" << RESET << std::endl;
@@ -109,9 +229,47 @@ std::ostream &operator<<(std::ostream &out, const ServerBlock &obj)
     out << "Name:  " << obj.getServerName() << std::endl;
 
     out << CYAN << "Root Dir" << RESET << std::endl;
-    std::string path = obj.getRootDir();
-    out << "Dir:  " << path << std::endl;
-    out << "Exist: " << PathChecking::exist(path) << "  IsDir:  " << PathChecking::isDirectory(path)
-        << "  R:  " << PathChecking::getReadPermission(path) << "  R:  " << PathChecking::getWritePermission(path) << "  R:  " << PathChecking::getExecPermission(path) << std::endl; 
+    std::string pathRoot = obj.getRootDir();
+    out << "Dir:  " << pathRoot << std::endl;
+    out << "Exist: " << PathChecking::exist(pathRoot) << "  IsDir:  " << PathChecking::isDirectory(pathRoot)
+        << "  R:  " << PathChecking::getReadPermission(pathRoot) << "  W:  " << PathChecking::getWritePermission(pathRoot) << "  X:  " << PathChecking::getExecPermission(pathRoot) << std::endl; 
+    
+    out << CYAN << "Index" << RESET << std::endl;
+    std::string pathIndex = obj.getIndex();
+    out << "Dir:  " << pathIndex << std::endl; 
+    out << "Exist: " << PathChecking::exist(pathIndex) << "  IsDir:  " << PathChecking::isFile(pathIndex)
+        << "  R:  " << PathChecking::getReadPermission(pathIndex) << "  W:  " << PathChecking::getWritePermission(pathIndex) << "  X:  " << PathChecking::getExecPermission(pathIndex) << std::endl; 
+    
+    out << CYAN << "Acces log" << RESET << std::endl;
+    std::string pathAccesLog = obj.getAccesLogFilePath();
+    out << "Dir:  " << pathAccesLog << std::endl;
+    try{
+        std::ofstream *logFile = obj.getAccessLogStream();
+        out << "Is open:  " << logFile->is_open() << "\n"
+            << "Test writing acces log: abc" << std::endl;
+        *logFile << "abc\n";
+        logFile->close();
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
+
+    out << CYAN << "Errors log" << RESET << std::endl;
+    std::string pathErrorsLog = obj.getErrorsLogFilePath();
+    out << "Dir:  " << pathErrorsLog << std::endl;
+    try{
+        std::ofstream *logFile = obj.getErrorsLogStream();
+        out << "Is open:  " << logFile->is_open() << "\n"
+            << "Test writing acces log: abc" << std::endl;
+        *logFile << "abc\n";
+        logFile->close();
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
+
+    out << CYAN << "Body Size Limit" << RESET << std::endl;
+    out << "Limit: " << obj.getBodySizeLimit() << std::endl;
+    
     return out;
 }
--- src/config/serverBlock.hpp
+++ src/config/serverBlock.hpp
@@ -6,19 +6,23 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 13:28:47 by lagea             #+#    #+#             */
-/*   Updated: 2024/12/17 22:19:00 by lagea            ###   ########.fr       */
+/*   Updated: 2024/12/18 19:03:40 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __SERVERBLOCK_HPP__
 # define __SERVERBLOCK_HPP__
 
+#define UINT16_MAX 65535
+
 #include <vector>
 #include <iostream>
 #include <map>
 #include <set>
-#include <limits>
 #include <algorithm>
+#include <fstream>
+#include <limits>
+#include "fcntl.h"
 
 #include "locationBlock.hpp"
 #include "tokenizer.hpp"
@@ -37,14 +41,20 @@ class ServerBlock
         std::vector<int> getListeningPortsVector() const;
         std::string getServerName() const;
         std::string getRootDir() const;
+        std::string getIndex() const;
+        std::string getAccesLogFilePath() const;
+        std::ofstream *getAccessLogStream() const;
+        std::string getErrorsLogFilePath() const;
+        std::ofstream *getErrorsLogStream() const;
+        int getBodySizeLimit() const;
         
     private:
         std::vector<int> _listeningports; //index 0 is default port
         std::string _servername;
         std::string _rootdir;
         std::string _index;
-        std::string _acceslogdir;
-        std::string _errorlogdir;
+        std::string _acceslogdpath;
+        std::string _errorlogpath;
         int _bodysizelimit;
         std::map<std::string ,class locationBlock> _locationblock;
         std::map<std::pair<int, std::string>, class locationBlock> _errorpages;
@@ -53,6 +63,10 @@ class ServerBlock
         void parseListeningPort(std::string &);
         void parseServerName(t_token &);
         void parseRootDir(t_token &);
+        void parseIndex(t_token &);
+        void parseAccesLogPath(t_token &);
+        void parseErrorsLogPath(t_token &);
+        void parseLimitBodySize(t_token &);
 };
 
 std::ostream &operator<<(std::ostream &, const ServerBlock &);
